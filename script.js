b561ca88c57d2f7895c45ea6f871af07bd9810d3
Taha Jumaah <taharasheed95@gmail.com>
2021-08-28 19:44:28 +0300

    Now the log shows which choice the computer picked, either rock, paper, or scissors.

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-function ComputerPlay(){
+let num = 0;
 
+
+
+
+function computerChoice() {
+    min = Math.ceil(1);
+    max = Math.floor(3);
+    num = Math.floor(Math.random() * (max - min + 1) + min);
+    console.log(num);
+    pickedHand();
+}
+
+
+function pickedHand() {
+    if (num === 1) {
+        console.log('Computer Picked Rock')
+    }
+
+    else 
+    if (num === 2) {
+        console.log('Computer Picked Paper')
+    }
+
+    else {
+        console.log('Computer Picked Scissors')
+    }
+    
 }
